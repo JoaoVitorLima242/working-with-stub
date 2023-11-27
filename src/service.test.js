cdb09ea77456bb3e0192df9d9ef0d8adb8dfbd75
JoaoVitorLima242 <jlima0500@gmail.com>
2023-11-26 22:16:00 -0300

    feat: adding sinon

--- src/service.test.js
+++ src/service.test.js
@@ -2,11 +2,11 @@ const Service = require('./service')
 const assert = require('assert')
 
 ;(async () => {
+    const service = new Service()
+    
     {
-        const service = new Service()
         const planetOne = await service.getPlanet(2)
 
-        console.log(JSON.stringify(planetOne))
-        
+        console.log(JSON.stringify(planetOne))    
     }
 })()
